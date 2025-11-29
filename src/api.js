5c08d5f5f646c91ea6e902c117e88a82890353ff
maheshwari <maheshwari200328@gmail.com>
2025-11-29 14:04:41 +0530

    login changing

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 const API=axios.create({
-      baseURL:'http://localhost:5000/api',
+      baseURL:'https://resetpasswordbackend.onrender.com/api',
 })
 export const registerUser=(userData)=>API.post('/register',userData);
 export const loginUser=(userData)=>API.post('/login',userData);
-export const forgotPassword=(email)=>API.post('https://resetpasswordbackend.onrender.com/api/forgot-password',{email});
-export const resetPassword=(token,password)=>API.post(`https://resetpasswordbackend.onrender.com/api/reset-password/${token}`,{password})
+export const forgotPassword=(email)=>API.post('/forgotpassword',{email});
+export const resetPassword=(token,password)=>API.post(`/resetpassword/${token}`,{password})
